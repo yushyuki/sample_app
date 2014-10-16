01b76ccf723f2fb6eda303e92191c19408d1bd9f
yushyuki <yu1590yu@gmail.com>
2014-10-16 11:11:04 +0900

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       # 保存の成功をここで扱う。
       redirect_to @user
